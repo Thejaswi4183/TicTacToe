4d00a69f49828aca6bd7f2069d0ba2acb05c355d
Thejaswi S <thejaswi4uns@gmail.com>
2024-10-06 00:07:41 +0530

    Modified

---
+++ app/src/main/java/net/myapplication/TicTacToe/MainActivity.java
@@ -0,0 +1,25 @@
+package net.myapplication.TicTacToe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+    public void playButtonClick(View view){
+        Intent intent=new Intent(this,PlayerSetup.class);
+        startActivity(intent);
+    }
+}
