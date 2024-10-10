7e952a82d6b1951cc7e02c7a5760a2732e3e869e
Thejaswi S <thejaswi4uns@gmail.com>
2024-10-10 21:21:59 +0530

    Modifed

--- app/src/main/java/net/myapplication/TicTacToe/PlayerSetup.java
+++ app/src/main/java/net/myapplication/TicTacToe/PlayerSetup.java
@@ -1,6 +1,9 @@
 package net.myapplication.TicTacToe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PlayerSetup extends AppCompatActivity {
-
+    private EditText player1;
+    private EditText player2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.player_setup);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        player1 =findViewById(R.id.player1Name);
+        player2 =findViewById(R.id.player2Name);
+    }
+    public void submitButtonClick(View view){
+        String player1Name = player1.getText().toString();
+        String player2Name = player2.getText().toString();
+
+        Intent intent=new Intent(this,GameDisplay.class);
+        intent.putExtra("PLAYER_NAMES",new String[] {player1Name,player2Name});
+        startActivity(intent);
     }
 }
