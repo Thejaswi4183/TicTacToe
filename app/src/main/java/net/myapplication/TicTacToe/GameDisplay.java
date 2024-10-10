bc9ad965dcc8b388e00aa069588df7f2973370f3
Thejaswi S <thejaswi4uns@gmail.com>
2024-10-11 00:33:43 +0530

    Modified

--- app/src/main/java/net/myapplication/TicTacToe/GameDisplay.java
+++ app/src/main/java/net/myapplication/TicTacToe/GameDisplay.java
@@ -6,12 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class GameDisplay extends AppCompatActivity {
 
